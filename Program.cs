5be377d6602b111e4db1181c8b45e84349328b53
MuhDavin213 <davin.new58@gmail.com>
2022-12-05 16:49:18 +0700

    add new stack

--- Program.cs
+++ Program.cs
@@ -49,13 +49,31 @@ namespace Exercise_4
             if (empty())
                 Console.WriteLine("\nStack Empty");
             else
-        }
-
-    }
-    internal class Program
-    {
+            {
+                for (tmp = top; tmp != null; tmp = tmp.next)
+                {
+                    Console.WriteLine(tmp.Davin);
+                }
+            }
+        }   
         static void Main(string[] args)
         {
+            Stack s = new Stack();
+            while (true)
+            {
+                Console.WriteLine("\n***Stack Menu");
+                Console.WriteLine("1. Push");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your choice: ");
+                string input = Console.ReadLine();
+                char ch = Convert.ToChar(input == "" ? "0" : input);
+                switch (ch)
+                {
+                    case
+                }
+            }
         }
     }
 }
